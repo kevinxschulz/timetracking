3591df7ad8a39f02c2abd50fe0dc1e33f93d8955
kevinxschulz <kevin.schulzpl@web.de>
2024-02-11 20:39:27 +0100

    added component structure

---
+++ timetracking-app/src/components/index.js
@@ -0,0 +1,6 @@
+// components/index.js
+export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as TimeEntryForm } from './TimeEntryForm/TimeEntryForm';
+export { default as TimeEntryList } from './TimeEntryList/TimeEntryList';
+export { default as CustomerList } from './CustomerList/CustomerList';
+export { default as ActivityList } from './ActivityList/ActivityList';
